437347e26fdaba1c2fa0ba747f30a462020f2d88
Khoeung-Sokhai <sokhaikhoeung@gmail.com>
2023-04-28 16:34:58 +0700

    long time7

--- src/main/java/CSX/MatchingEngine/app/MatchingEngineApplication.java
+++ src/main/java/CSX/MatchingEngine/app/MatchingEngineApplication.java
@@ -1,37 +1,37 @@
 package CSX.MatchingEngine.app;
 
 import CSX.MatchingEngine.app.service.MessageService;
+import CSX.MatchingEngine.app.websocket.etc.RawSocketHandler;
+import CSX.MatchingEngine.app.websocket.send.QuotationDataSending;
+import org.springframework.boot.SpringApplication;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-
 public class MatchingEngineApplication {
-
-
     private static final int TCP_PORT = 5000;
 
+    public static QuotationDataSending quotationDataSending;
+    public static RawSocketHandler rawSocketHandler;
+
+
     public static void main(String[] args) {
+//        BaseResponse response = new BaseResponse();
 
-
-        BaseResponse response = new BaseResponse();
         HashMap<String, List<Order>> hm = new HashMap<>();
-
+        HashMap<Integer, List<Execution>> ex = new HashMap<>();
         MessageService messageService = new MessageService();
-
         String tcpString = "";
-
         try {
             messageService.start(TCP_PORT);
             while (true) {
                 // System.out.println("Great! Get Message Successfully\uD83E\uDD24");
+                System.out.println(" ");
                 tcpString = messageService.getMsg();
                 System.out.println("Message: " + tcpString);
-                int i = 0;
                 List<Order> orderList = new ArrayList<>();
-
-
                 Order order = new Order();
                 //Sub String From Message Get from TCP broker
                 order.issueCode = tcpString.substring(2, 12);
@@ -42,7 +42,7 @@ public class MatchingEngineApplication {
                 order.orderUV = Integer.parseInt(tcpString.substring(42, 52));
                 order.orderDate = Integer.parseInt(tcpString.substring(52, 60));
 
-                String key = order.issueCode + order.orderType + order.orderUV + order.accountNo;
+                String key = order.issueCode + order.orderUV + order.orderType;
 
                 if (hm.get(key) == null) {
                     orderList.add(order);
@@ -50,30 +50,30 @@ public class MatchingEngineApplication {
                     orderList = hm.get(key);
                     orderList.add(order);
                 }
-
                 hm.put(key, orderList);
 
+                //Test String
 
-//                    quotationDataSending.sending(rawSocketHandler);
-//Test String
-//                System.out.println("issueCode: " + hm.get(key).get(0).issueCode);
-//                System.out.println("orderType: " + hm.get(key).get(0).orderType);
-//                System.out.println("brokerId : " + hm.get(key).get(0).brokerId);
-//                System.out.println("accountNo: " + hm.get(key).get(0).accountNo);
-//                System.out.println("orderQty : " + hm.get(key).get(0).orderQty);
-//                System.out.println("orderUV : " + hm.get(key).get(0).orderUV);
-//                System.out.println("orderDate: " + hm.get(key).get(0).orderDate);
+                System.out.println("issueCode: " + hm.get(key).get(0).issueCode);
+                System.out.println("orderType: " + hm.get(key).get(0).orderType);
+                System.out.println("brokerId : " + hm.get(key).get(0).brokerId);
+                System.out.println("accountNo: " + hm.get(key).get(0).accountNo);
+                System.out.println("orderQty : " + hm.get(key).get(0).orderQty);
+                System.out.println("orderUV  : " + hm.get(key).get(0).orderUV);
+                System.out.println("orderDate: " + hm.get(key).get(0).orderDate);
 
                 System.out.println("Initial list of elements: " + hm);
-//                    return response;
+                int sum  = 0;
+                for (Order ord : hm.get(key)) {
+                    sum += ord.orderQty;
+                    System.out.println("Total Order quantity: " + sum);
+                }
+//sum all quality for each of key
             }
         } catch (Exception e) {
-            System.out.println(e);
+            messageService.stop();
         }
-
-//		quotationDataSending.sending(rawSocketHandler);
-
-
+//        quotationDataSending.sending(rawSocketHandler,"work");
     }
 
 
@@ -85,8 +85,10 @@ public class MatchingEngineApplication {
         int orderUV;
         int orderQty;
         int orderDate;
+    }
 
-
+    static class Execution {
+        int executionQty;
     }
 
 }
--- src/main/java/CSX/MatchingEngine/app/websocket/send/QuotationDataSending.java
+++ src/main/java/CSX/MatchingEngine/app/websocket/send/QuotationDataSending.java
@@ -1,8 +1,12 @@
 package CSX.MatchingEngine.app.websocket.send;
 
 import CSX.MatchingEngine.app.websocket.etc.RawSocketHandler;
+
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
 
 import static java.lang.String.format;
 
@@ -10,33 +14,32 @@ import static java.lang.String.format;
 @Log4j2
 public class QuotationDataSending {
 
-    public void sending(RawSocketHandler rawSocketHandler) {
-        String msgSend = "Its work";
+    public void sending(RawSocketHandler rawSocketHandler,String args) {
+        String msgSend = "Work";
         log.info("Send growth board market data [{}]", msgSend); //console.log in intellij
-            rawSocketHandler.sendAll(msgSend);
-
+            rawSocketHandler.sendAll(args);
 //        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-//        String formattedDate = sdf.format(broker.getOrderDate());
+//        String formattedDate = sdf.format(Order.getOrderDate());
 //
-//        if (broker.getOrderType() == 1) {
-//            String msgSend =    "A1"                                                      +
-//                                format("%09d",broker.getIssueCode())                      +
-//                                format("%04d",Integer.parseInt(broker.getBrokerId()))     +
-//                                format("%04d",Integer.parseInt(broker.getAccountNo()))    +
-//                                format("%08d",broker.getOriginalOrderNo())                +
-//                                format("%08d", broker.getOrderUV())                       +
-//                                format("%08d", broker.getOrderUV())                       + formattedDate;
+//        if (Order.getOrderType() == 1) {
+//            String msgSend =    "A1"                                                     +
+//                                Order.getIssueCode()                                     +
+//                                format("%04d",Integer.parseInt(Order.getBrokerId()))     +
+//                                format("%04d",Integer.parseInt(Order.getAccountNo()))    +
+//                                format("%08d",Order.getOriginalOrderNo())                +
+//                                format("%08d", Order.getOrderUV())                       +
+//                                format("%08d", Order.getOrderUV())                       + formattedDate;
 //            log.info("Send growth board market data [{}]", msgSend); //console.log in intellij
 //            rawSocketHandler.sendAll(msgSend);
 //        }
-//        if (broker.getOrderType() == 2) {
-//            String msgSend =    "A2"                                                      +
-//                                format("%09d",broker.getIssueCode())                      +
-//                                format("%04d",Integer.parseInt(broker.getBrokerId()))     +
-//                                format("%04d",Integer.parseInt(broker.getAccountNo()))    +
-//                                format("%08d",broker.getOriginalOrderNo())                +
-//                                format("%08d", broker.getOrderUV())                       +
-//                                format("%08d", broker.getOrderUV())                       + formattedDate;
+//        if (Order.getOrderType() == 2) {
+//            String msgSend =    "A2"                                                     +
+//                                Order.getIssueCode()                                     +
+//                                format("%04d",Integer.parseInt(Order.getBrokerId()))     +
+//                                format("%04d",Integer.parseInt(Order.getAccountNo()))    +
+//                                format("%08d",Order.getOriginalOrderNo())                +
+//                                format("%08d", Order.getOrderUV())                       +
+//                                format("%08d", Order.getOrderUV())                       + formattedDate;
 //            log.info("Send growth board market data [{}]", msgSend); //console.log in intellij
 //            rawSocketHandler.sendAll(msgSend);
 //        }
